94a1eaa3778ced59963f0a95e39a37a63918d550
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2022-10-23 08:58:03 -0700

    commit tree 7784eda9f8149d6df2184a193ff3bb3d97b0f7b4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name){
+    return array.filter(possibleMatch => possibleMatch.toLowerCase() === name.toLowerCase())
+  }
+
+  function fuzzyMatch(array, starting){
+    return array.filter(function(item){
+      return item.startsWith(starting)
+    })
+    
+  }
+
+  function matchName(array, name){
+    return array.filter(function(item){
+      if(name === item.name){
+        return true
+      }
+    })
+  }
